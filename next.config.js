1b45f678785f24af3034aa29c6cec18ba6478750
Aakash Thakur <thakurakash321@gmail.com>
2023-08-22 22:39:38 +0530

    feat: project setip

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  swcMinify: true,
+  images: {
+    domains: [`${process.env.DOMAIN}`, "place-hold.it"],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
